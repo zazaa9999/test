2d3ff347a77ff7d259dbadc4c399187ac9015b9b
zazaa9999 <33926313+zazaa9999@users.noreply.github.com>
2017-11-23 17:04:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,47 @@
 <?php
-curl -X POST \
--H 'Content-Type:application/json' \
--H 'Authorization: Bearer {XAWCotmUZKAUYUCDq+5fWv9+x3Tfq5OqAnh2ZwJs87MPxJifNJDbXaytleeGdZxS66ifQOL38Lm+szCINlTmiCYEpQNHC48849i486FGDQqamIipYskbifuHWptKYUXeNN6PI439E01d0hOq4BAKGQdB04t89/1O/w1cDnyilFU=}' \
--d '{
-    "replyToken":"nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
-    "messages":[
-        {
-            "type":"text",
-            "text":"Hello, user"
-        },
-        {
-            "type":"text",
-            "text":"May I help you?"
-        }
-    ]
-}' https://api.line.me/v2/bot/message/reply
-?>
+$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+	}
+}
+echo "OK";
